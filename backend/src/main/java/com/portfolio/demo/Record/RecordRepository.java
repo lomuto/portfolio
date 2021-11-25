28291bc2f2d30b0099cf72db64715c0b616ff6a3
lomuto <kangys1245@gmail.com>
2021-11-25 15:32:07 +0900

    feature (RecordRepository.java) Add repository

--- backend/src/main/java/com/portfolio/demo/Record/RecordRepository.java
+++ backend/src/main/java/com/portfolio/demo/Record/RecordRepository.java
@@ -1,2 +1,6 @@
-package com.portfolio.demo.Record;public interface RecordRepository {
+package com.portfolio.demo.Record;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecordRepository extends JpaRepository {
 }
