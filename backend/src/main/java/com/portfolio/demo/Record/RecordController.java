2b152aa921ebfeca0a8c1a1dab13b2c1bfdae9ab
lomuto <kangys1245@gmail.com>
2021-11-25 15:43:56 +0900

    feature (RecordService.java) Add RecordController

--- backend/src/main/java/com/portfolio/demo/Record/RecordController.java
+++ backend/src/main/java/com/portfolio/demo/Record/RecordController.java
@@ -1,2 +1,36 @@
-package com.portfolio.demo.Record;public class RecordController {
+package com.portfolio.demo.Record;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/record")
+public class RecordController {
+
+    private RecordService recordService;
+
+    @Autowired
+    public RecordController(RecordService recordService) {
+        this.recordService = recordService;
+    }
+
+    @GetMapping("/records")
+    public ResponseEntity<List<Record>> getRecords() {
+        return new ResponseEntity<List<Record>>(recordService.getRecords(), HttpStatus.OK);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Record> addRecord(@RequestBody Record record) {
+        // validation
+        recordService.addRecord(record);
+        return new ResponseEntity<Record>(record, HttpStatus.CREATED);
+    }
 }
