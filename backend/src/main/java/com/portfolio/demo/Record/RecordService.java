fd9955303076aa6ef754f0be83091b9c3532026b
lomuto <kangys1245@gmail.com>
2021-11-25 15:43:45 +0900

    feature (RecordService.java) Add RecordService

--- backend/src/main/java/com/portfolio/demo/Record/RecordService.java
+++ backend/src/main/java/com/portfolio/demo/Record/RecordService.java
@@ -1,2 +1,25 @@
-package com.portfolio.demo;public class RecordService {
+package com.portfolio.demo.Record;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RecordService {
+
+    private RecordRepository repository;
+
+    @Autowired
+    public RecordService(RecordRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Record> getRecords() {
+        return repository.findAll();
+    }
+
+    public void addRecord(Record record) {
+        repository.save(record);
+    }
 }
