fd866bf8fe0c2056d8795c494ae9fa681193abe6
lomuto <kangys1245@gmail.com>
2021-11-25 21:02:33 +0900

    feature (Record.js) Add Record class

---
+++ frontend/src/Record.js
@@ -0,0 +1,15 @@
+class Record {
+    constructor(
+        title,
+        skillEarned,
+        startDate,
+        endDate,
+        comments
+    ) {
+        this.title = title,
+        this.skillEarned = skillEarned,
+        this.startDate = startDate,
+        this.endDate =  endDate,
+        this.comment = comments
+    }
+}
